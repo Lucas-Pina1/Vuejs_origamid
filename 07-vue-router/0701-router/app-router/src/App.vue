f4c58ddcb80498b24fa5f4d2fff18760c81dc1ae
Lucas Pina <ti.lucaspina@gmail.com>
2022-04-05 05:22:34 -0300

    Vue router

---
+++ 07-vue-router/0701-router/app-router/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1 @click="mostrarRouter">Logo</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/Cursos">Cursos</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    mostrarRouter() {
+      console.log(this.$route);
+    },
+  },
+};
+</script>
+
+<style>
+a {
+  padding: 10px;
+}
+
+.router-link-exact-active {
+  color: #84e;
+  font-weight: bold;
+}
+</style>
