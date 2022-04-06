848f3ec45cc42a7cf1d28afdc6b0466cd2e885ef
Lucas Pina <ti.lucaspina@gmail.com>
2022-04-06 09:00:34 -0300

    techno cursos vue router

---
+++ 07-vue-router/techno-cursos/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import api from "../services/axios";
+export default {
+  name: "home",
+  data() {
+    return {
+      api: null,
+    };
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get(api)
+        .then((r) => r.json)
+        .then((r) => {
+          this.api = r;
+        });
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
+</script>
